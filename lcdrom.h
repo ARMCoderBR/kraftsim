1d0de89cefd18fe5276611847c34c0d160408418
Milton Maldonado Jr. <milton@cpstecnologia.com.br>
2026-01-20 11:01:40 -0300

    LCDROM

---
+++ lcdrom.h
@@ -0,0 +1,15 @@
+/*
+ * lcdrom.h
+ *
+ *  Created on: 20 de jan. de 2026
+ *      Author: milton
+ */
+
+#ifndef LCDROM_H_
+#define LCDROM_H_
+
+#include <stdint.h>
+
+extern uint8_t lcdrom[];
+
+#endif /* LCDROM_H_ */
